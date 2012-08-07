776eacf547691d2878e4ace40536b357d061cd62
Paul Sarando <psarando@iplantcollaborative.org>
2012-08-06 17:39:12 -0700

    CORE-3389 Refactored PropertyTypeCategory.
    Updated PropertyTypeCategory to accept a "value_type" code from the property-types service, as well as a display string, in its constructor.
    Added a getValueType method to access this value.

--- src/main/java/org/iplantc/core/metadata/client/property/PropertyTypeCategory.java
+++ src/main/java/org/iplantc/core/metadata/client/property/PropertyTypeCategory.java
@@ -3,21 +3,25 @@ package org.iplantc.core.metadata.client.property;
 import java.util.Collection;
 
 /**
- * An enum to represent property types
+ * An enum to represent property types. The constructor takes a "value_type" string, which must match a
+ * value returned by the property-types service, as well a display string for displaying to the user in
+ * the UI.
  * 
  * @author sriram
- *
+ * 
  */
 public enum PropertyTypeCategory {
-    INPUT(DataObject.INPUT_TYPE), 
-    STRING("String"), 
-    NUMBER("Number"), 
-    BOOLEAN("Boolean"), 
-    OUTPUT(DataObject.OUTPUT_TYPE);
+    INPUT(DataObject.INPUT_TYPE, DataObject.INPUT_TYPE),
+    STRING("String", "String"),
+    NUMBER("Number", "Number"),
+    BOOLEAN("Boolean", "Boolean"),
+    OUTPUT(DataObject.OUTPUT_TYPE, DataObject.OUTPUT_TYPE);
 
+    private String valueType;
     private String displayText;
 
-    private PropertyTypeCategory(String displayText) {
+    private PropertyTypeCategory(String valueType, String displayText) {
+        this.valueType = valueType;
         this.displayText = displayText;
     }
 
@@ -29,11 +33,15 @@ public enum PropertyTypeCategory {
      */
     public boolean isOneOf(Collection<String> categoryStrings) {
         for (String s : categoryStrings) {
-            if (s.toUpperCase().equals(name())) {
+            if (s.equals(valueType)) {
                 return true;
             }
         }
         return false;
+    }
+
+    public String getValueType() {
+        return valueType;
     }
 
     @Override
