cyverse-archive/ui-metadata-module

commit dea624845246a585e5b40297a9f1f8bcc2a512e6

CORE-3410 Added DataObject null-check in Property::toJson.
This was causing a NPE when attempting to save window state with an App Launch window open with an Output property type.

src/main/java/org/iplantc/core/metadata/client/property/Property.java
@@ -210,7 +210,7 @@ public class Property extends JSONMetaDataObject {
         json.put(OMIT_IF_BLANK, JSONBoolean.getInstance(omit_if_blank));
 
         if (DataObject.INPUT_TYPE.equalsIgnoreCase(type)
-                || DataObject.OUTPUT_TYPE.equalsIgnoreCase(type)) {
+                || DataObject.OUTPUT_TYPE.equalsIgnoreCase(type) && dataObject != null) {
             json.put(DATA_OBJECT, dataObjectToJson());
         }
 

commit 7b8f71062cc50da606004ed30d4a7c42385f172b

CORE-3410 Fixed DataObject null-check in Property::toJson.
Fixed logic error introduced in dea624845246a585e5b40297a9f1f8bcc2a512e6.

src/main/java/org/iplantc/core/metadata/client/property/Property.java
@@ -208,7 +208,7 @@ public class Property extends JSONMetaDataObject {
         json.put(OMIT_IF_BLANK, JSONBoolean.getInstance(omit_if_blank));
 
         if (DataObject.INPUT_TYPE.equalsIgnoreCase(type)
-                || DataObject.OUTPUT_TYPE.equalsIgnoreCase(type) && dataObject != null) {
+                || DataObject.OUTPUT_TYPE.equalsIgnoreCase(type)) {
             json.put(DATA_OBJECT, dataObjectToJson());
         }
 
@@ -216,6 +216,10 @@ public class Property extends JSONMetaDataObject {
     }
 
     protected JSONObject dataObjectToJson() {
+        if (dataObject == null) {
+            return null;
+        }
+
         dataObject.setName(getLabel());
         dataObject.setLabel(getLabel());
         dataObject.setCmdSwitch(getName());