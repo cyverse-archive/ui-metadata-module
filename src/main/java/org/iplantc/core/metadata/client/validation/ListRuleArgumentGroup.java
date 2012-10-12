465eff5c02004371722eb42d55f2d9089dd4e53f
Paul Sarando <psarando@iplantcollaborative.org>
2012-10-12 16:36:09 -0700

    CORE-3517 Added isSingleSelect boolean to ListRuleArgumentGroup.
    This boolean tells the UI that only one item in this group's children is allowed to be selected.

--- src/main/java/org/iplantc/core/metadata/client/validation/ListRuleArgumentGroup.java
+++ src/main/java/org/iplantc/core/metadata/client/validation/ListRuleArgumentGroup.java
@@ -2,6 +2,8 @@ package org.iplantc.core.metadata.client.validation;
 
 import java.util.List;
 
+import com.google.web.bindery.autobean.shared.AutoBean.PropertyName;
+
 /**
  * An AutoBean interface for ListRuleArgument groups.
  * 
@@ -17,4 +19,10 @@ public interface ListRuleArgumentGroup extends ListRuleArgument {
     public List<ListRuleArgument> getArguments();
 
     public void setArguments(List<ListRuleArgument> arguments);
+
+    @PropertyName("isSingleSelect")
+    public boolean isSingleSelect();
+
+    @PropertyName("isSingleSelect")
+    public void setSingleSelect(boolean isSingleSelect);
 }
